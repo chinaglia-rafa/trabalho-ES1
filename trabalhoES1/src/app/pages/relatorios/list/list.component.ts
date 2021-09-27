2ab2cb623a4f27603946f46dab57e1c9859e3b8e
Rafael Araujo Chinaglia <chinaglia.rafa@gmail.com>
2021-09-27 16:39:18 -0300

    feat(relatorio): nome dos alunos na visão de orientador

--- trabalhoES1/src/app/pages/relatorios/list/list.component.ts
+++ trabalhoES1/src/app/pages/relatorios/list/list.component.ts
@@ -9,6 +9,7 @@ import { UserDataServiceService } from 'src/app/services/userDataService/user-da
 export class ListComponent implements OnInit {
 
   public reports: any = [];
+  public isLeader: boolean = false;
 
   constructor(private userDataService: UserDataServiceService) { }
 
@@ -16,6 +17,7 @@ export class ListComponent implements OnInit {
 
   ngAfterViewInit(): void {
     this.userDataService.userDataObservable.subscribe((data: any) => {
+      this.isLeader = data.type == 'leader';
       console.log('List of reports:', data.reports);
       this.reports = data.reports;
     });
